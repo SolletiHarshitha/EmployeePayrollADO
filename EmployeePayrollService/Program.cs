c8d303651d8b6d143a3868f5a70a021779253ccd
Solleti Harshitha <solletiharshitha@gmail.com>
2021-08-07 00:47:14 +0530

    [Harshitha] Add employee using threads

--- EmployeePayrollService/Program.cs
+++ EmployeePayrollService/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EmployeePayrollService
 {
@@ -26,6 +27,15 @@ namespace EmployeePayrollService
                 Console.WriteLine("Successfully removed");
             else
                 Console.WriteLine("Not removed");
+
+            ThreadOperations threadOperations = new ThreadOperations();
+            List<EmployeeDetails> employeeList = new List<EmployeeDetails>();
+            //Adding employee details to the list
+            employeeList.Add(details);
+            //Add list of employee details without thread
+            threadOperations.AddEmployeeWithoutThread(employeeList);
+            //Add list of employee details with thread
+            threadOperations.AddEmployeeWithThread(employeeList);
         }
     }
 }
